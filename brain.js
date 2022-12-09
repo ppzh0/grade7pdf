20de26219246add8d534d03f28c667354d66d3d6
ppzh0 <narrtor06@gmail.com>
2022-12-09 11:11:22 +0630

    this one is the version, main contents aren't aligned to left

---
+++ brain.js
@@ -0,0 +1,27 @@
+function math1 () {
+  window.open('data/Math/Grade-7-Math-Algebra.pdf')
+}
+function math2 () {
+  window.open('data/Math/Grade-7-Math-Geometry.pdf')
+}
+function science () {
+  window.open('data/Science/Grade-7-Science.pdf')
+}
+function mm1 () {
+  window.open('data/Myanmar/Grade-7-Myanmar-Grammer.pdf')
+}
+function mm2 () {
+  window.open('data/Myanmar/Grade-7-Myanmar-Novels.pdf')
+}
+function mm3 () {
+  window.open('data/Myanmar/Grade-7-Myanmar-Poems.pdf')
+}
+function geo () {
+  window.open('data/Geography/Grade-7-Geography.pdf')
+}
+function english () {
+  window.open('data/English/Grade-7-English.pdf')
+}
+function discord() {
+  window.open('https://discord.gg/yzJaDr7DyQ');
+}
